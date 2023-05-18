9367c9a2ad86b9ea569fbb11cb5852b5f0de45ac
anna <annabarellaalonso@gmail.com>
2023-05-18 14:57:24 +0200

    A-fetching working

---
+++ src/components/Band.js
@@ -0,0 +1,12 @@
+
+
+function Band(props) {
+    return (
+        <article>
+            <p>{props.name}</p>
+        </article>
+
+    )
+}
+
+export default Band
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import Band from "@/components/Band";
 
-export default function Home() {
+export default function Home({ data }) {
+
+  console.log(data)
   return (
     <>
       <Head>
@@ -8,6 +11,34 @@ export default function Home() {
       </Head>
       <h1>Hello thi is Anna</h1>
       <h2>Trying server side fetching</h2>
+      <h3>The banda are:</h3>
+      <div>
+        {data.map(band => <Band key={band.name} {...band} />)}
+        {/*  <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      </div>
+
+
+
     </>
   );
+}
+
+export async function getStaticProps() {
+  // Fetch data from external API
+  const api = "https://blush-entertaining-raver.glitch.me/bands"
+  const res = await fetch(api);
+  // If no data - no page (404)
+  if (res.status != 200) {
+    return {
+      notFound: true,
+    };
+  }
+  const data = await res.json();
+  ;
+  // Pass data to the page via props
+  return {
+    props: {
+      data
+    }
+  };
 }
