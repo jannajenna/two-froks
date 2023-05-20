67fcb34a85f294244dce284c2b03bcb7e07c3598
natalia <nata774e@stud.kea.dk>
2023-05-20 20:06:13 +0200

    added some pages and colors (attempt google fonts)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "@/styles/fonts.js";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
---
+++ src/styles/fonts.js
@@ -0,0 +1,16 @@
+import { Grandstander, Space_Grotesk, Poppins } from "next/font/google";
+
+export const grandstander = Grandstander({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const space_grotesk = Space_Grotesk({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+});
