2cfaf789121347028ce598ea28001998fa607e56
natalia <nata774e@stud.kea.dk>
2023-05-23 11:19:11 +0200

    making the tent accordion fetch dynamic data

--- src/pages/tickets/index.js
+++ src/pages/tickets/index.js
@@ -1,4 +1,5 @@
 import Accordion from "@/components/Accordion/Accordion";
+import TentAccordion from "@/components/Accordion/TentAccordion";
 import { TicketData } from "@/data/ticketData";
 import { TentData } from "@/data/tentData";
 import { GreenData } from "@/data/greenData";
@@ -6,7 +7,7 @@ import { Hero } from "@/components/Hero/Hero";
 import styles from "./Tickets.module.css";
 import GreenButton from "@/components/Buttons/GreenButton";
 
-const Tickets = () => {
+export default function Tickets(props) {
   return (
     <>
       <Hero title="Tickets" />
@@ -20,8 +21,8 @@ const Tickets = () => {
           Tents<span className={styles.priceHint}> FROM 299,-/stk</span>
         </h2>
         <div className={styles.accordion}>
-          {TentData.map(({ title, content }) => (
-            <Accordion title={title} content={content} />
+          {props.tents.map((tent) => (
+            <TentAccordion title={tent.area} content={tent.available} />
           ))}
         </div>
         <h2>Sustainability</h2>
@@ -37,6 +38,23 @@ const Tickets = () => {
       </div>
     </>
   );
-};
+}
 
-export default Tickets;
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const api = "https://blush-entertaining-raver.glitch.me/available-spots";
+  const res = await fetch(api);
+  // If no data - no page (404)
+  if (res.status != 200) {
+    return {
+      notFound: true,
+    };
+  }
+  const data = await res.json();
+  // Pass data to the page via props
+  return {
+    props: {
+      tents: data,
+    },
+  };
+}
